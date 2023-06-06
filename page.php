a5e133c6df1cf06ecefb03b7bde4edea5bbf6a23
devahmad899 <132460671+ttl-ahmadsaqib@users.noreply.github.com>
2023-06-06 16:16:10 +0500

    updated blocks

--- page.php
+++ page.php
@@ -49,13 +49,13 @@ $hdfall_pagetitle = glide_page_title('step_spbop_title');
 
 <section id="page-section" class="page-section">
 	<!-- Content Start -->
-
+<div class="container">
 	<?php while ( have_posts() ) { the_post();
 		//Include specific template for the content.
 		get_template_part( 'partials/content', 'page' );
 
 	} ?>
-
+</div>
 	<div class="clear"></div>
 	<div class="ts-80"></div>
 
